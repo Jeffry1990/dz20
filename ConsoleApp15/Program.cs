fbad3121452910bb5ce6f8681c75fd7dbdeda328
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-15 13:38:56 +0300

    Add files via upload

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i < 93; i +=7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
